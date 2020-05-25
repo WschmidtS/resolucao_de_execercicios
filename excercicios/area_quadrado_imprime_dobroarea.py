9f31c833ebc2a49f3f73992fcf91cfffec418082
Walter Schmidt Silva <walter@tecacademy.com.br>
2020-05-25 11:54:16 -0300

    Codado um programa para imprimir o dobro da área de um quadrado
    close #8

---
+++ excercicios/area_quadrado_imprime_dobroarea.py
@@ -0,0 +1,7 @@
+'''
+Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
+'''
+print('Cálcumo da área de um quadrado imprimindo o dobro dessa área')
+lado_quadrado = float(input('Tamanho do lado do Quadrado em cm:'))
+resultado = (lado_quadrado**2) * 2
+print(f'O dobro da área do quadrado é: {resultado} cm²')
