6bfe0459d7bc93be762b858e71c84580a9e90ac6
Walter Schmidt Silva <walter@tecacademy.com.br>
2020-05-25 12:07:35 -0300

    Codado um programa para calcular o salário de acordo com o valor e quantidade de horas trabalhadas
    close #9

---
+++ excercicios/calculo_salario_por_hora_trabalhada.py
@@ -0,0 +1,10 @@
+'''
+Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês.
+Calcule e mostre o total do seu salário no referido mês.
+'''
+
+print('Cálculo de salário por horas trabalhadas')
+valor_hora = float(input('Digite o valor pago por hora trabalhada em R$:'))
+qtd_horas_trabalhadas = float(input('Digite a quantidade de horas trabalhadas no mês:'))
+salario = valor_hora * qtd_horas_trabalhadas
+print('O seu salário nesse mês é de R$ {:.2f}'.format(salario))
