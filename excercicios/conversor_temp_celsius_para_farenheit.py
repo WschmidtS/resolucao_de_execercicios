2e8bda4f43351718a3d1cf0b5db77e4f79d8c550
Walter Schmidt Silva <walter@tecacademy.com.br>
2020-06-01 20:52:23 -0300

    Codado um programa para converter a temperatura de Farenheit para Celsius
    close #10

---
+++ excercicios/conversor_temp_celsius_para_farenheit.py
@@ -0,0 +1,8 @@
+'''
+Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Farenheit.
+'''
+
+print('Conversor de Temperatura de Celsius para Farenheit ')
+celsius = float(input('Digite a temperatura em Celsius:'))
+farenheit = 1.88 * celsius + 32
+print(f'A temperatura em Farenheit é {farenheit:.2f} Fº')
