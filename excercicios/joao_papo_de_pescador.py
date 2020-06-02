ebede280360bccf3f70c1ab42f1b8a9aa994a333
Walter Schmidt Silva <walter@tecacademy.com.br>
2020-06-01 21:49:36 -0300

    Codado um programa para calcular a multa por excesso de peso na pesca segundo o regulamento do Estado de SP.
    close #15

---
+++ excercicios/joao_papo_de_pescador.py
@@ -0,0 +1,16 @@
+"""
+João Papo-de-Pescador, homem de bem, comprou um microcomputador para controlar o rendimento diário de seu trabalho.
+Toda vez que ele traz um peso de peixes maior que o estabelecido pelo regulamento de pesca do estado de
+São Paulo (50 quilos) deve pagar uma multa de R$ 4,00 por quilo excedente. João precisa que você faça um programa
+que leia a variável peso (peso de peixes) e calcule o excesso. Gravar na variável excesso a quantidade de quilos
+além do limite e na variável multa o valor da multa que João deverá pagar.
+Imprima os dados do programa com as mensagens adequadas.
+"""
+peso = float(input('Digite quantos Kilos de peixe você pescou: '))
+excesso = peso - 50
+if excesso > 0:
+    multa = excesso * 4.00
+    print(f'Você ultrapassou o limite do Regulamento de Pesca do Estado de SP.\n'
+          f'A sua multa a pagar é: R$ {multa:.2f} ')
+elif excesso <= 0:
+    print(f'Você não excedeu o peso do Regulamento de Pesca do Estado de SP.')
